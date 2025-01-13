f4f3e19bc1ab064dc20ee5510cd4d42408921765
Joel Mora <joeljmorab@gmail.com>
2025-01-13 09:44:28 -0300

    crud laravel con vue primera practica

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,26 @@
 import './bootstrap';
+
+import { createApp, h } from 'vue';
+import { createInertiaApp, Link } from '@inertiajs/vue3';
+import vuetify from '../Plugins/vuetify.js';
+import Layout from './Components/Layout.vue';
+
+createInertiaApp({
+  resolve: name => {
+    const pages = import.meta.glob('../Pages/**/*.vue', { eager: true });
+
+    let page = pages[`../Pages/${name}.vue`];
+    if (!page) {
+      throw new Error(`Page not found: ${name}`);
+    }
+    page.default.layout = page.default.layout || Layout;
+    return page;
+  },
+  setup({ el, App, props, plugin }) {
+    createApp({ render: () => h(App, props) })
+      .use(plugin)
+      .use(vuetify)
+      .component('Link', Link)
+      .mount(el);
+  },
+});
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\CommentController;
+use App\Models\Comment;
+use Inertia\Inertia;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/inicio', [CommentController::class, 'index'])->name('inicio');
